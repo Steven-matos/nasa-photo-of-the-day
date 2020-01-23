2de0991feb6c69e3eb302f8186310c9e646702c1
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-01-23 17:02:33 -0500

    added Styling to title

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,15 +3,26 @@ import CardInfo from '../CardInfo/CardInfo';
 import styled from 'styled-components';
 
 const CardImage = styled.img`
-    width: 90%
+    width: 90%;
+    border-radius: .5rem;
+    padding-top: 1rem;
+`;
+
+const CardTitleContainer = styled.div `
+padding: 1rem 0;
+background: #3C215E;
+color: white;
+
 `;
 
 
 const Card = (props) => {
     return (
-        <div className="card-container">
-            <h1>Nasa's Image of the Day</h1>
-            <h2>{props.title}</h2>
+        <div>
+            <CardTitleContainer>
+                <h1>Nasa's Image of the Day</h1>
+                <h2>{props.title}</h2>
+            </CardTitleContainer>
             <CardImage src={props.image} alt="Nasa pic of the day" />
             <CardInfo 
                 descInfo={props.desc}
