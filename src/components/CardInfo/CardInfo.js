af4c3fb7f8bddd125210ff73b355935cf55c7a2b
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-01-23 16:31:05 -0500

    Centered info section under card image

--- src/components/CardInfo/CardInfo.js
+++ src/components/CardInfo/CardInfo.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+ 
+const InfoContainer = styled.div`
+width: 800px;
+text-align: center;
+margin: 0 auto;
+padding-top: 2rem;
+
+`;
 
 const CardInfo = (props) => {
 
     console.log(props)
 
     return (
-        <div className="info-container">
+        <InfoContainer>
             <p>{props.descInfo}</p>
             <p>Image Copyright: {props.copyrightInfo}</p>
-        </div>
+        </InfoContainer>
     );
 };
 
