9fef4d98aa07ac0c56361631a4b0fc6fee34755a
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-01-23 16:55:57 -0500

    rearranged h1 tag into card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import CardInfo from '../CardInfo/CardInfo';
+import styled from 'styled-components';
+
+const CardImage = styled.img`
+    width: 90%
+`;
+
 
 const Card = (props) => {
     return (
         <div className="card-container">
+            <h1>Nasa's Image of the Day</h1>
             <h2>{props.title}</h2>
-            <img src={props.image} alt="Nasa pic of the day" />
+            <CardImage src={props.image} alt="Nasa pic of the day" />
             <CardInfo 
                 descInfo={props.desc}
                 copyrightInfo={props.copyright}
