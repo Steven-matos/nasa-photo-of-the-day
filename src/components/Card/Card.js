f8c89f605b37f987bcb99e93727408f4f058d223
Steven A. Matos <steven_matos@ymail.com>
2024-08-20 00:17:45 -0400

    optimize page and update nasa logo and hide api key

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,54 +1,56 @@
-import React from 'react';
-import CardInfo from '../CardInfo/CardInfo';
-import styled from 'styled-components';
+import React from "react";
+import CardInfo from "../CardInfo/CardInfo";
+import styled from "styled-components";
 
 const CardImage = styled.img`
-    width: 90%;
-    border-radius: .5rem;
-    padding-top: 1.5rem;
+  width: 90%;
+  border-radius: 0.5rem;
+  padding-top: 1.5rem;
 `;
 
-const CardTitleContainer = styled.div `
-    padding: 1rem 0;
-    background: #3C215E;
-    color: white;
-    img {
-        width: 15%;
-        @media(max-width: 768px) {
-            width: 30%;
+const CardTitleContainer = styled.div`
+  padding: 1rem 0;
+  background: #3c215e;
+  color: white;
+  img {
+    width: 15%;
+    @media (max-width: 768px) {
+      width: 30%;
     }
-  }  
+  }
 `;
 
-const CardContainer = styled.div `
-background-color: white;
+const CardContainer = styled.div`
+  background-color: white;
 `;
 
-const CardTitle = styled.h1 `
-    font-family: 'nasalization';
-    @media(max-width: 768px) {
-            padding: 0 1.5rem;
-    }
+const CardTitle = styled.h1`
+  font-family: "nasalization";
+  @media (max-width: 768px) {
+    padding: 0 1.5rem;
+  }
 `;
-
 
 const Card = (props) => {
-    return (
-        <CardContainer>
-            <CardTitleContainer>
-                <img src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png" alt="Nasa Logo"/>
-                <CardTitle>Image of the Day!</CardTitle>
-            </CardTitleContainer>
-            <CardImage src={props.image} alt="Nasa pic of the day" />
-            <CardInfo 
-                descInfo={props.desc}
-                copyrightInfo={props.copyright}
-                titleInfo={props.title}
-                dateInfo={props.date}
-                changeDate={props.changeDate}
-            />
-        </CardContainer>
-    );
+  return (
+    <CardContainer>
+      <CardTitleContainer>
+        <img
+          src="https://www.nasa.gov/wp-content/themes/nasa/assets/images/nasa-logo.svg"
+          alt="Nasa Logo"
+        />
+        <CardTitle>Image of the Day!</CardTitle>
+      </CardTitleContainer>
+      <CardImage src={props.image} alt="Nasa pic of the day" />
+      <CardInfo
+        descInfo={props.desc}
+        copyrightInfo={props.copyright}
+        titleInfo={props.title}
+        dateInfo={props.date}
+        changeDate={props.changeDate}
+      />
+    </CardContainer>
+  );
 };
 
 export default Card;
--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,70 +1,69 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios';
-import Card from '../Card/Card';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "../Card/Card";
 import styled from "styled-components";
 
-const NasaKey = process.env.REACT_APP_API_KEY
+const NasaKey = process.env.REACT_APP_NASA_API_KEY;
 
 const Container = styled.div`
-position: absolute;
-left: 200px;
-text-align: center;
-max-width: 1000px;
-margin: 3.5rem auto;
-box-shadow: 5px 5px 5px 5px black;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 850px;
+  text-align: center;
+  margin: 15rem auto;
+  box-shadow: 5px 5px 5px 5px black;
 
-     @media(max-width: 768px) {
+  @media (max-width: 768px) {
     max-width: 350px;
-    margin: 4rem auto;
-    left: 130px;
-  }
-
-  @media(max-width: 1080px) {
-    width: 850px;
-    left: 40px;
   }
 `;
 
-
 const CardContainer = () => {
-    const moment = require('moment');
+  const moment = require("moment");
 
-    const [nasaCard, setNasaCard] = useState([]);
-    const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
+  console.log(NasaKey);
 
-    useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=eaCdXvdhcYLYoDbpsy0HdjT1rooeMEtfbfpiPKfT&date=${date}`)
-        .then(res => {
-            setNasaCard(res.data)
-        })
-        .catch(err => {
-            console.error("You are getting an error of: ", err); 
-        })
-    }, [date])
+  const [nasaCard, setNasaCard] = useState([]);
+  const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
 
-    const changeDate = e => {
-        e.preventDefault();
-        let calender = e.target.value;
-        if (!moment(calender, 'YYYY-MM-DD').isValid()){
-            console.log('Please enter a correct Date Format YYYY-MM_DD');
-        } else {
-            setDate(calender);
-        }
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=${NasaKey}&date=${date}`
+      )
+      .then((res) => {
+        setNasaCard(res.data);
+      })
+      .catch((err) => {
+        console.error("You are getting an error of: ", err);
+      });
+  }, [date]);
+
+  const changeDate = (e) => {
+    e.preventDefault();
+    let calender = e.target.value;
+    if (!moment(calender, "YYYY-MM-DD").isValid()) {
+      console.log("Please enter a correct Date Format YYYY-MM_DD");
+    } else {
+      setDate(calender);
     }
+  };
 
-    return (
-        <Container>
-            <Card 
-                Key={nasaCard.date}
-                title={nasaCard.title}
-                image={nasaCard.hdurl}
-                desc={nasaCard.explanation}
-                copyright={nasaCard.copyright}
-                date={nasaCard.date}
-                changeDate={changeDate}
-            />    
-        </Container>
-    );
+  return (
+    <Container>
+      <Card
+        Key={nasaCard.date}
+        title={nasaCard.title}
+        image={nasaCard.hdurl}
+        desc={nasaCard.explanation}
+        copyright={nasaCard.copyright}
+        date={nasaCard.date}
+        changeDate={changeDate}
+      />
+    </Container>
+  );
 };
 
 export default CardContainer;
